a59664955b19584b21eb13c902d70c0d6773718e
kenu <kenu.heo@gmail.com>
2022-01-26 23:37:10 +0900

    VITE env #3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
   function getHello() {
     const greet = document.getElementById('greet')
-    fetch('http://localhost:4000/api/hello')
+    fetch(import.meta.env.VITE_API_SERVER + '/api/hello')
       .then(response => response.json())
       .then(data => greet.innerHTML = JSON.stringify(data))
   }
